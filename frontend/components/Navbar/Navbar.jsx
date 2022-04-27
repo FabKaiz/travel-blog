d00588d908faf7b2ce36f67b9d9c1b0b5585a8a0
Fabien Chareun <cacrfabfr@gmail.com>
2022-04-27 11:28:54 +0200

    navbar done

--- frontend/components/Navbar/Navbar.jsx
+++ frontend/components/Navbar/Navbar.jsx
@@ -9,12 +9,22 @@ const Navbar = () => {
       <div className={styles.nav__item_container}>
         <Link href="/">
           <a href="">
-            <Image src={logo} alt="Tracel Blog Logo" width={148} height={148} />
+            <Image src={logo} alt="Tracel Blog Logo" width={140} height={140} />
           </a>
         </Link>
       </div>
       <div className={styles.nav__item_container}>
-        <p>TODO ITEMS</p>
+        <p>
+          Blog made by{' '}
+          <a
+            href="https://github.com/FabKaiz"
+            target="_blank"
+            rel="noreferrer"
+            style={{ color: '#919C8F' }}
+          >
+            FabKaiz
+          </a>
+        </p>
       </div>
     </div>
   )
