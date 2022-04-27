72c8b2a170daee8a98d10fbf075fb9619b8a68d7
Fabien Chareun <cacrfabfr@gmail.com>
2022-04-27 12:36:15 +0200

    added loading bar with styles

--- frontend/components/Navbar/Navbar.jsx
+++ frontend/components/Navbar/Navbar.jsx
@@ -5,26 +5,34 @@ import styles from './Navbar.module.css'
 
 const Navbar = () => {
   return (
-    <div className={styles.nav__container}>
-      <div className={styles.nav__item_container}>
-        <Link href="/">
-          <a href="">
-            <Image src={logo} alt="Tracel Blog Logo" width={140} height={140} />
-          </a>
-        </Link>
-      </div>
-      <div className={styles.nav__item_container}>
-        <p>
-          Blog made by{' '}
-          <a
-            href="https://github.com/FabKaiz"
-            target="_blank"
-            rel="noreferrer"
-            style={{ color: '#919C8F' }}
-          >
-            FabKaiz
-          </a>
-        </p>
+    <div>
+      <div className="loading"></div>
+      <div className={styles.nav__container}>
+        <div className={styles.nav__item_container}>
+          <Link href="/">
+            <a href="">
+              <Image
+                src={logo}
+                alt="Tracel Blog Logo"
+                width={140}
+                height={140}
+              />
+            </a>
+          </Link>
+        </div>
+        <div className={styles.nav__item_container}>
+          <p>
+            Blog made by{' '}
+            <a
+              href="https://github.com/FabKaiz"
+              target="_blank"
+              rel="noreferrer"
+              style={{ color: '#919C8F' }}
+            >
+              FabKaiz
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   )
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,15 @@
+import Router from 'next/router'
 import '../styles/globals.css'
 import Navbar from '../components/Navbar/Navbar'
+import NProgress from 'nprogress'
 
 const MyApp = ({ Component, pageProps }) => {
+  NProgress.configure({ showSpinner: false })
+  NProgress.configure({ parent: '.loading' })
+
+  Router.events.on('routeChangeStart', () => NProgress.start())
+  Router.events.on('routeChangeComplete', () => NProgress.done())
+
   return (
     <div className="app__container">
       <Navbar />
